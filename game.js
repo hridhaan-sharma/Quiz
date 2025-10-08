8fd574a29117b4c35864448ce5a022698f864743
hridhaan-sharma <84317830+hridhaan-sharma@users.noreply.github.com>
2025-10-08 18:34:05 +0530

    Update game.js

--- game.js
+++ game.js
@@ -83,7 +83,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem('mostRecentScore', score);
 
-    return window.location.assign('./end.html');
+    return window.location.assign('./Quiz/end.html');
   }
 
   questionCounter++;
@@ -141,4 +141,5 @@ decrementScore = (num) => {
 };
 
 startGame();
+
 
